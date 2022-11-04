58cae8bfbffa5f409000791746f4ae6f712acf84
Chaltu Abdulkadir <chaltuabdulkadir@Chaltus-MacBook-Air.local>
2022-11-04 18:14:29 -0500

    added encapsulated Class attributes with getters/setter methods

--- src/AirBnB/Property.java
+++ src/AirBnB/Property.java
@@ -1,5 +1,113 @@
 package AirBnB;
 
+
 public class Property {
 
+    private String address;
+    private int id;
+    private String name;
+    private String city;
+    private int zipcode;
+    private int bedrooms;
+    private int bathrooms;
+    private double rate;
+    private int guestCount;
+
+
+    public Property(int id, String name, String address, String city, int zipcode, int bedrooms, int bathrooms, int guestCount, double rate) {
+        this.id = id;
+        this.address = address;
+        this.name = name;
+        this.city = city;
+        this.zipcode = zipcode;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.rate = rate;
+        this.guestCount = guestCount;
+
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public void setBedrooms(int bedrooms) {
+        this.bedrooms = bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public void setBathrooms(int bathrooms) {
+        this.bathrooms = bathrooms;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+
+    public int getGuestCount() {
+        return guestCount;
+    }
+
+    public void setGuestCount(int guestCount) {
+        this.guestCount = guestCount;
+    }
+
+    public void print(){
+        System.out.println("\nProperty id: " + this.id);
+        System.out.println("Welcome to " + this.name);
+        System.out.println("Our address is: " + this.address +", " + this.city + " " + ""+ this.zipcode);
+        System.out.println("Our property has " + ""+ this.bedrooms + " bedrooms, " + this.bathrooms + " bathrooms and can accommodate " + this.guestCount + " guests");
+        System.out.println("Nightly rate is:" + " $"+this.rate);
+
+    }
+
+
 }
+
